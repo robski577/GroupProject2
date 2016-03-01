3117ad7a84c0533330558eeeb0a3fad2a847405e
casey <casey.holmgren@gmail.com>
2016-02-29 21:14:54 -0600

    started on ListView for the Menu
    program is crashing when trying to implement custom cells for the
    ListView. It is acting as if there is a broken link, but none can be
    found.

--- GroupProject2/GroupProject2/MenuViewController.swift
+++ GroupProject2/GroupProject2/MenuViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var menuTableView: UITableView!
+    
+    var items = ["Chef's Salad","Chocolate Milk Shake","Super Burger"]
+    var descriptions = ["You'll have to ask the chef. He's on break.","Mostly milk","Instead of buns, you get two mini pizzas!"]
+    var prices = ["7.95","5.00","12.95"]
+    var photos = [UIImage()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +26,19 @@ class MenuViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.menuTableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! MenuItemCell
+        
+        cell.item.text = items[indexPath.row]
+        cell.desc.text = descriptions[indexPath.row]
+        cell.price.text = prices[indexPath.row]
+        return cell
     }
     
 
