5e6f08711760e474d0cbf7757b00494d13dfd38e
FreeMasen <RFM@localhost>
2016-03-01 21:37:14 -0600

    things and stuff

--- GroupProject2/GroupProject2/Item.swift
+++ GroupProject2/GroupProject2/Item.swift
@@ -14,13 +14,30 @@ class Item {
     var Price: Double
     var Desc: String
     var TimeAddedToOrder: NSDate?
-    var Menues: [Bool]
+    var Menues: MenuType
+    var Type: ItemType
     
-    init(id: Int, name: String, price: Double, desc: String, menues: [Bool]) {
+    init(id: Int, name: String, price: Double, desc: String, menues: MenuType, type: ItemType) {
         Id = id
         Name = name
         Price = price
         Desc = desc
         Menues = menues
+        Type = type
     }
+}
+
+enum MenuType: Int {
+    case Nothing =      0
+    case Breakfast =    1
+    case Lunch =        2
+    case HappyHour =    4
+    case Dinner =       8
+}
+
+enum ItemType: Int {
+    case Nothing =      0
+    case Apps =         1
+    case Entre =        2
+    case Desert =       4
 }
